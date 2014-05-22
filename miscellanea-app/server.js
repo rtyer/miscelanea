bfdebc83ca508da6c971f807f8ea98e8756b759b
Ryan Tyer <ryan.tyer@jivesoftware.com>
2014-05-21 20:44:44 -0600

    playing with orchestrate

--- miscellanea-app/server.js
+++ miscellanea-app/server.js
@@ -3,7 +3,9 @@
 
 const express    = require('express'),
 	  app        = express(), 
-	  bodyParser = require('body-parser');
+	  bodyParser = require('body-parser'),
+	  config	 = require('./config'),
+	  db 		 = require('orchestrate')(config.orchestrate.token);
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
@@ -12,10 +14,33 @@ app.use(bodyParser());
 const router = express.Router();
 
 router.get("/", function(req, res){
-	res.json({message: "welcome to the miscellanea"});
+	db.newEventBuilder()
+		.from('hits', 'test')
+		.type('hit')
+		.time(Date.now())
+		.data({test: "text"})
+		.then(function (res) {
+      		console.log(res.statusCode);
+    	});
+
+	const today 	= Date.now(),
+	      yesterday = new Date(today - 24*60*60*1000 ).getTime();
+
+	const hits 		= db.newEventReader()
+						.from('hits', 'test')
+						.start(yesterday)
+						.end(today)
+						.type('hits')
+						.then(function(r){
+							res.json({message: "welcome to the miscellanea", "today": today, "yesterday":yesterday, "results": r.body});
+						})
+						.fail(function(err){
+							res.send(500, err);
+						});	
+	
 });
 
-//put everything of /api for now
+//put everything off /api for now
 app.use("/api", router); 
 
 const port = process.env.PORT || 8888;
